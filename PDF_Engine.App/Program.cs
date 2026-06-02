e9ae918c77ac0f6f79d770915be46cf4367afd6b
Romain Lecesne <niamor2305@gmail.com>
2026-06-02 16:09:40 +0200

    Project & Solution names corrected

--- PDF_Engine.App/Program.cs
+++ PDF_Engine.App/Program.cs
@@ -1,2 +1,35 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+using PDF_Engine.Core; 
+
+namespace PDF_Engine.App
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("=== PDF_Engine : Sandbox Testing ===");
+            Console.WriteLine("Testing feature: Merge (PDF_Merger)");
+
+            List<string> filesToMerge = new List<string> 
+            { 
+                "test1.pdf", 
+                "test2.pdf" 
+            };
+            string outputFile = "resultat_fusion.pdf";
+
+            PDF_Merger merger = new PDF_Merger();
+            
+            try
+            {
+                Console.WriteLine("Merging in progress...");
+                merger.Merge(filesToMerge, outputFile);
+                Console.WriteLine($"-> Success! File saved as: {outputFile}");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"-> [Error] Merge failed: {ex.Message}");
+            }
+        }
+    }
+}
--- PDF_Engine.Core/PDF_Merger.cs
+++ PDF_Engine.Core/PDF_Merger.cs
@@ -1,11 +1,30 @@
 ﻿using System;
-using System.IO;
+using System.Collections.Generic;
 using PdfSharp.Pdf;
 using PdfSharp.Pdf.IO;
 
-namespace PDF_Engine.Core;
-
-public class PDF_Merger
+namespace PDF_Engine.Core
 {
-
+    public class PDF_Merger 
+    {
+        public void Merge(List<string> inputPaths, string outputPath)
+        {
+            using (PdfDocument outputDocument = new PdfDocument())
+            {
+                foreach (string file in inputPaths)
+                {
+                    using (PdfDocument inputDocument = PdfReader.Open(file, PdfDocumentOpenMode.Import))
+                    {
+                        int count = inputDocument.PageCount;
+                        for (int idx = 0; idx < count; idx++)
+                        {
+                            PdfPage page = inputDocument.Pages[idx];
+                            outputDocument.AddPage(page);
+                        }
+                    }
+                }
+                outputDocument.Save(outputPath);
+            }
+        }
+    }
 }
